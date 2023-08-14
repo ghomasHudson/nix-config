608df392e0a2c8528b9863babf813cf0808f3285
Thomas Hudson <g.t.hudson@durham.ac.uk>
2023-08-14 18:25:32 +0100

    Commentin out

--- hosts/wilfred/default.nix
+++ hosts/wilfred/default.nix
@@ -21,7 +21,7 @@
   ];
 
   boot = {
-    kernelPackages = pkgs.linuxKernel.packages.linux_zen;
+    #kernelPackages = pkgs.linuxKernel.packages.linux_zen;
     binfmt.emulatedSystems = [ "aarch64-linux" "i686-linux" ];
   };
 
